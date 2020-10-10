85871666a615d6eae3ccfa1143759bc473c6a989
olets123 <47143886+olets123@users.noreply.github.com>
2020-10-10 15:24:32 +0200

    fixes

--- src/App.js
+++ src/App.js
@@ -213,18 +213,7 @@ function App() {
           }
 
       </div>
-      <InstagramEmbed
-        url='https://instagr.am/p/Zw9o4/'
-        maxWidth={320}
-        hideCaption={false}
-        containerTagName='div'
-        protocol=''
-        injectScript
-        onLoading={() => {}}
-        onSuccess={() => {}}
-        onAfterRender={() => {}}
-        onFailure={() => {}}
-      />
+      
 
       
 
